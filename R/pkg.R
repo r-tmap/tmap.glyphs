4757dd94140c0f1d473c85c1242ea23acd4548b8
mtennekes <mtennekes@gmail.com>
2025-05-05 10:21:22 +0200

    ...

---
+++ R/pkg.R
@@ -0,0 +1,11 @@
+#' Extension for tmap: glyphs can be created
+#'
+#' New layer functions available for 'tmap' that are used to create glyph maps.
+#'
+#' @author Martijn Tennekes \email{mtennekes@@gmail.com}
+#' @concept GIS
+#' @concept thematic maps
+#' @concept statistical maps
+#' @concept choropleth
+#' @concept bubble map
+"_PACKAGE"
